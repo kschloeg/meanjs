5fa0fe9a5feff1c152e37fb33cbfb2b57b024878
Kirk Schloegel <kschloeg@gmail.com>
2016-06-02 15:35:27 -0500

    Remove swagger --> working e2e

--- src/api/v1/product/ProductController.ts
+++ src/api/v1/product/ProductController.ts
@@ -2,7 +2,6 @@ import _ = require('lodash');
 import async = require('async');
 import express = require('express');
 import http_status = require('http-status');
-import route = require('../../../util/swagger/routes');
 
 import {ProductManager, ProductInterface, Product, ProductStatus} from 'productts';
 
@@ -10,12 +9,13 @@ import ProductResponse = require('./ProductResponse');
 import Permissions = require('../../permissions/Permissions');
 
 class ProductController {
-    @route({
-        path: "/v1/product",
-        method: "POST",
-        body: { name: true }
-    })
-    public static createProduct(req:express.Request, res:express.Response) {
+    // @route({
+    //     path: "/v1/product",
+    //     method: "POST",
+    //     body: { name: true }
+    // })
+    public static createProduct(req: express.Request, res: express.Response) {
+        console.log("  KIRK createProduct " + JSON.stringify(req.params));
         var product_data: ProductInterface = new Product(<ProductInterface>req.body);
         product_data.status = ProductStatus.ACTIVE;
 
@@ -27,20 +27,27 @@ class ProductController {
             // hydrate: ['create', (cb, results) => ProductHydrator.hydrateProduct(results.create, cb)],
             response: ['create', (cb, results) => cb(null, ProductResponse.formatProductResponse(results.create))]
         }, (err, results: any) => {
+            console.log("    KIRK " + JSON.stringify(results.permissions));
+            console.log("    KIRK " + JSON.stringify(results.create));
+            console.log("    KIRK " + JSON.stringify(results.response));
             if (err) {
+              console.log("    KIRK ERR " + JSON.stringify(err));
+
                 // return Check.sendError(res, err);
             }
             return res.json(results.response);
         });
     }
 
-    @route({
-        path: "/v1/product/{product_id}",
-        method: "GET",
-        query: { include_inactive: false }
-    })
-    public static getProductById(req:express.Request, res:express.Response) {
-        var product_id = req.params.product_id;
+    // @route({
+    //     path: "/v1/product/{product_id}",
+    //     method: "GET",
+    //     query: { include_inactive: false }
+    // })
+    public static getProductById(req: express.Request, res: express.Response) {
+      console.log("  KIRK getProductById " + JSON.stringify(req.params));
+        console.log("  KIRK getProductById " + JSON.stringify(req.query));
+        var product_id = req.params.id;
         var include_inactive = req.query.include_inactive;
 
         ProductManager.helloWorld();
@@ -51,7 +58,13 @@ class ProductController {
             // hydrate: ['permissions', (cb, results) => ProductHydrator.hydrateProduct(results.product, cb)],
             response: ['permissions', (cb, results) => cb(null, ProductResponse.formatProductResponse(results.product))]
         }, (err, results: any) => {
+          console.log("    KIRK " + JSON.stringify(product_id));
+          console.log("    KIRK " + JSON.stringify(results.product));
+          console.log("    KIRK " + JSON.stringify(results.permissions));
+          console.log("    KIRK " + JSON.stringify(results.response));
             if (err) {
+              console.log("    KIRK ERR " + JSON.stringify(err));
+
                 // return Check.sendError(res, err);
             }
             return res.json(results.response);
